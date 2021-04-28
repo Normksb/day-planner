00f2096989063c84a0f53cf0a9cea35417da6cf6
Norman Bernard <nksb414@gmail.com>
2021-04-28 10:46:06 +0800

    added buttons and completed saving to local storage

--- script.js
+++ script.js
@@ -25,16 +25,58 @@ var button15 = $('#button15');
 var button16 = $('#button16');
 var button17 = $('#button17');
 
+textArea9.val()
+
 // button9.addEventListener("click",saveTextArea9);
 
 button9.on('click', function () {
-    var textArea9Value = textArea9.value
-    console.log(textArea9Value)
-    localStorage.setItem("textArea9Value",textArea9Value)
+    console.log(textArea9.val())
+        localStorage.setItem("textArea9Value",textArea9.val())
 });
 
-function saveTextArea9(){
-    var textArea9Value = textArea9.textArea9.value
-    console.log(textArea9Value)
-    localStorage.setItem("textArea9Value",textArea9Value)
-}
+button10.on('click', function () {
+    console.log(textArea9.val())
+        localStorage.setItem("textArea10Value",textArea10.val())
+});
+
+button11.on('click', function () {
+    console.log(textArea9.val())
+        localStorage.setItem("textArea11Value",textArea11.val())
+});
+
+button12.on('click', function () {
+    console.log(textArea9.val())
+        localStorage.setItem("textArea12Value",textArea12.val())
+});
+
+button13.on('click', function () {
+    console.log(textArea9.val())
+        localStorage.setItem("textArea13Value",textArea13.val())
+});
+
+button14.on('click', function () {
+    console.log(textArea9.val())
+        localStorage.setItem("textArea14Value",textArea14.val())
+});
+
+button15.on('click', function () {
+    console.log(textArea9.val())
+        localStorage.setItem("textArea15Value",textArea15.val())
+});
+
+button16.on('click', function () {
+    console.log(textArea9.val())
+        localStorage.setItem("textArea16Value",textArea16.val())
+});
+
+button17.on('click', function () {
+    console.log(textArea9.val())
+        localStorage.setItem("textArea17Value",textArea17.val())
+});
+
+
+//function saveTextArea9(){
+//    var textArea9Value = textArea9.textArea9.value
+//    console.log(textArea9Value)
+//    localStorage.setItem("textArea9Value",textArea9Value)
+//}
